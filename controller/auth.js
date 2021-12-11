f4921c717e72a0d962baf58619d7000985f33a37
amit thakur <amithakur081@gmail.com>
2021-12-11 22:22:20 +0530

    fix : find user

--- controller/auth.js
+++ controller/auth.js
@@ -8,18 +8,21 @@ const register = async(req,res)=>{
     let {username , email , password , dateOfBirth } = req.body;
     try{
         let userByName = await User.findOne({username  : username});
-        console.log(userByName)
+        
         let userByEmail = await User.findOne({email : email});
-        console.log(userByEmail)
+        
         if(userByName || userByEmail){
             setResponse(res,400,'User already exist')
         }
 
         password = bcrypt.hashSync(password,10);
         console.log(password)
-        const newUser = await new User({username,email,password,dateOfBirth})
+        const newUser = await new User({username,email,password,dateOfBirth ,bio : "",website : ""})
         await newUser.save();
-        setResponse(res,200,"Registeration successful")
+
+        const token =  jwt.sign({_id : newUser._id},process.env.JWT_SECRET);
+        const data  = await newUser.populate("post followers following savePost blockList");
+        setResponse(res,200,"Registeration successful",{token , data})
 
     }catch(err){
         setResponse(res,500,err.message)
